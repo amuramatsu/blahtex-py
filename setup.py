d1072d03cff57c6545a35c730075c29ad57947d6
MURAMATSU Atsushi <amura@tomato.sakura.ne.jp>
2020-05-13 17:46:42 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,141 @@
+#! /usr/bin/env python3
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
+
+__version__ = '0.0.1'
+
+
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked. """
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include()
+
+ext_modules = [
+    Extension(
+        'blahtex',
+        # Sort input source files to ensure bit-for-bit reproducible builds
+        # (https://github.com/pybind/python_example/pull/53)
+        sorted([
+            'src/blahtex.cpp',
+	    'blahtexml/Source/BlahtexCore/InputSymbolTranslation.cpp',
+	    'blahtexml/Source/BlahtexCore/Interface.cpp',
+	    'blahtexml/Source/BlahtexCore/LayoutTree.cpp',
+	    'blahtexml/Source/BlahtexCore/MacroProcessor.cpp',
+	    'blahtexml/Source/BlahtexCore/Manager.cpp',
+	    'blahtexml/Source/BlahtexCore/Parser.cpp',
+	    'blahtexml/Source/BlahtexCore/ParseTree1.cpp',
+	    'blahtexml/Source/BlahtexCore/ParseTree2.cpp',
+	    'blahtexml/Source/BlahtexCore/ParseTree3.cpp',
+	    'blahtexml/Source/BlahtexCore/MathmlNode.cpp',
+	    'blahtexml/Source/BlahtexCore/Token.cpp',
+	    'blahtexml/Source/BlahtexCore/XmlEncode.cpp',
+        ]),
+        include_dirs=[
+            # Path to pybind11 headers
+            get_pybind_include(),
+            "blahtexml/Source",
+            "blahtexml/Source/BlahtexCore",
+        ],
+        language='c++'
+    ),
+]
+
+# cf http://bugs.python.org/issue26689
+def has_flag(compiler, flagname):
+    """Return a boolean indicating whether a flag name is supported on
+    the specified compiler.
+    """
+    import tempfile
+    import os
+    with tempfile.NamedTemporaryFile('w', suffix='.cpp', delete=False) as f:
+        f.write('int main (int argc, char **argv) { return 0; }')
+        fname = f.name
+    try:
+        compiler.compile([fname], extra_postargs=[flagname])
+    except setuptools.distutils.errors.CompileError:
+        return False
+    finally:
+        try:
+            os.remove(fname)
+        except OSError:
+            pass
+    return True
+
+
+def cpp_flag(compiler):
+    """Return the -std=c++[11/14/17] compiler flag.
+
+    The newer version is prefered over c++11 (when it is available).
+    """
+    #flags = ['-std=c++17', '-std=c++14', '-std=c++11']
+    flags = ['-std=c++11']
+
+    for flag in flags:
+        if has_flag(compiler, flag):
+            return flag
+
+    raise RuntimeError('Unsupported compiler -- at least C++11 support '
+                       'is needed!')
+
+
+class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+    c_opts = {
+        'msvc': ['/EHsc', '/DWCHAR_T_IS_16BIT'],
+        'unix': [],
+    }
+    l_opts = {
+        'msvc': [],
+        'unix': [],
+    }
+
+    if sys.platform == 'darwin':
+        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+        c_opts['unix'] += darwin_opts
+        l_opts['unix'] += darwin_opts
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        link_opts = self.l_opts.get(ct, [])
+        if ct == 'unix':
+            opts.append(cpp_flag(self.compiler))
+            if has_flag(self.compiler, '-fvisibility=hidden'):
+                opts.append('-fvisibility=hidden')
+            if has_flag(self.compiler, '-Wno-deprecated-declarations'):
+                opts.append('-Wno-deprecated-declarations')
+            if has_flag(self.compiler, '-Wno-reorder'):
+                opts.append('-Wno-reorder')
+            if has_flag(self.compiler, '-Wno-unreachable-code'):
+                opts.append('-Wno-unreachable-code')
+
+        for ext in self.extensions:
+            ext.define_macros = [
+                ('VERSION_INFO',
+                 '"{}"'.format(self.distribution.get_version()))
+            ]
+            ext.extra_compile_args = opts
+            ext.extra_link_args = link_opts
+        build_ext.build_extensions(self)
+
+setup(
+    name='blahtex',
+    version=__version__,
+    author='MURAMATSU Atsushi',
+    author_email='amura@tomato.sakura.ne.jp',
+    url='https://github.com/amuramatsu/blahtex-py',
+    description='A python binding of blahtex',
+    long_description='',
+    ext_modules=ext_modules,
+    setup_requires=['pybind11>=2.5.0'],
+    cmdclass={'build_ext': BuildExt},
+    zip_safe=False,
+)
---
+++ src/blahtex.cpp
@@ -0,0 +1,102 @@
+
+#include <BlahtexCore/Interface.h>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(blahtex, m) {
+    m.doc() = "Blahtex binding for python";
+
+    static py::exception<blahtex::Exception> ex(m, "BlahtexException");
+    py::register_exception_translator([](std::exception_ptr p){
+	try {
+	    if (p) std::rethrow_exception(p);
+	} catch (const blahtex::Exception &e) {
+	    std::wstring msg = e.GetCode() + L": ";
+	    bool firstarg = true;
+	    for (std::wstring arg: e.GetArgs()) {
+		if (! firstarg)
+		    msg += L", ";
+		msg += arg;
+		firstarg = false;
+	    }
+	    PyErr_SetObject(ex.ptr(),
+			    PyUnicode_FromWideChar(msg.c_str(), msg.size()));
+	}
+    });
+    
+    py::class_<blahtex::Interface>(m, "Blahtex")
+	.def(py::init<>())
+	.def("process_input",
+	     &blahtex::Interface::ProcessInput,
+	     py::arg("input"), py::arg("display_style") = false)
+	.def("get_mathml", &blahtex::Interface::GetMathml)
+	.def("get_purified_tex", &blahtex::Interface::GetPurifiedTex)
+	.def("get_purified_tex_only", &blahtex::Interface::GetPurifiedTexOnly)
+	.def_readwrite("mathml_options",
+		       &blahtex::Interface::mMathmlOptions)
+	.def_readwrite("encoding_options",
+		       &blahtex::Interface::mEncodingOptions)
+	.def_readwrite("purified_tex_options",
+		       &blahtex::Interface::mPurifiedTexOptions)
+	.def_readwrite("texvc_compatibililty",
+		       &blahtex::Interface::mTexvcCompatibility)
+	.def_readwrite("indented", &blahtex::Interface::mIndented);
+
+    py::class_<blahtex::MathmlOptions> mathml_options(m, "MathmlOptions");
+    mathml_options
+	.def(py::init<>())
+	.def_readwrite("spacing_control",
+		       &blahtex::MathmlOptions::mSpacingControl)
+	.def_readwrite("use_version1_font_attributes",
+		       &blahtex::MathmlOptions::mUseVersion1FontAttributes)
+	.def_readwrite("allow_plane1",
+		       &blahtex::MathmlOptions::mAllowPlane1);
+    py::enum_<blahtex::MathmlOptions::SpacingControl>(mathml_options,
+						   "SpacingControl")
+	.value("STRICT",
+	       blahtex::MathmlOptions::SpacingControl::cSpacingControlStrict)
+	.value("MODERATE",
+	       blahtex::MathmlOptions::SpacingControl::cSpacingControlModerate)
+	.value("RELAXED",
+	       blahtex::MathmlOptions::SpacingControl::cSpacingControlRelaxed)
+	.export_values();
+    
+    py::class_<blahtex::EncodingOptions> encoding_options(m, "EncodingOptions");
+    encoding_options
+	.def(py::init<>())
+	.def_readwrite("mathml_encoding",
+		       &blahtex::EncodingOptions::mMathmlEncoding)
+	.def_readwrite("other_encoding_raw",
+		       &blahtex::EncodingOptions::mOtherEncodingRaw)
+	.def_readwrite("allow_plane1",
+		       &blahtex::EncodingOptions::mAllowPlane1);
+    py::enum_<blahtex::EncodingOptions::MathmlEncoding>(encoding_options,
+						       "MathmlEncoding")
+	.value("RAW",
+	       blahtex::EncodingOptions::MathmlEncoding::cMathmlEncodingRaw)
+	.value("NUMERIC",
+	       blahtex::EncodingOptions::MathmlEncoding::cMathmlEncodingNumeric)
+	.value("SHORT",
+	       blahtex::EncodingOptions::MathmlEncoding::cMathmlEncodingShort)
+	.value("LONG",
+	       blahtex::EncodingOptions::MathmlEncoding::cMathmlEncodingLong)
+	.export_values();
+
+    py::class_<blahtex::PurifiedTexOptions>(m, "PurifiedTexOptions")
+	.def(py::init<>())
+	.def_readwrite("display_math",
+		       &blahtex::PurifiedTexOptions::mDisplayMath)
+	.def_readwrite("allow_ucs",
+		       &blahtex::PurifiedTexOptions::mAllowUcs)
+	.def_readwrite("allow_cjk",
+		       &blahtex::PurifiedTexOptions::mAllowCJK)
+	.def_readwrite("allow_preview",
+		       &blahtex::PurifiedTexOptions::mAllowPreview)
+	.def_readwrite("japanese_font",
+		       &blahtex::PurifiedTexOptions::mJapaneseFont)
+	.def_readwrite("latex_preamble",
+		       &blahtex::PurifiedTexOptions::mLaTeXPreamble)
+	.def_readwrite("latex_before_math",
+		       &blahtex::PurifiedTexOptions::mLaTeXBeforeMath);
+}	
